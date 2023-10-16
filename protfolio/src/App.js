6de1df3d0d05e5df1cff9f5d055fed87014e4b82
Pepo Banchev <banchevpepo@gmail.com>
2023-10-17 00:52:57 +0300

    first upload
    added Navbar with bootstrap

---
+++ protfolio/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './Components/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+function App() {
+  return (
+
+    <Navbar />
+  );
+}
+
+export default App;
