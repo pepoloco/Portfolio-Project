2d9080638c56cb01f420f73d35f12003e53c8079
Pepo Banchev <banchevpepo@gmail.com>
2023-10-19 21:40:00 +0300

    update
    removed Particles , due hard time to work with it , my react is newer version

--- protfolio/src/App.js
+++ protfolio/src/App.js
@@ -1,20 +1,20 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-// import ParticlesAnimation from './Components/Particles';
-import Navbar from './Components/Navbar'
-import Header from './Components/Header';
+  import './App.css';
+  import 'bootstrap/dist/css/bootstrap.min.css'
+  import Navbar from './Components/Navbar'
+  import Header from './Components/Header';
+  import AppParticles from './Components/Particles';
 
 
 
-function App() {
-  return (
-    <>
-      {/* <ParticlesAnimation/> */}
-      <Navbar />
-      <Header />
-    </>
-  );
-}
+  function App() {
+    return (
+      <>
+        <AppParticles />
+        <Navbar />
+        <Header />
+      </>
+    );
+  }
 
 
-export default App;
+  export default App;
--- protfolio/src/Components/Particles.jsx
+++
@@ -1,5 +0,0 @@
-// import React from 'react';
-// import Particles from 'react-particles-js';
-
-
-// export default ParticleAnimation;
