ee634b57b8d10c5826e1f99843dd78d2fb77ea51
Pepo Banchev <banchevpepo@gmail.com>
2023-10-22 14:07:51 +0300

    update
    added Navbar

---
+++ Portfolio/src/components/Navbar/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { getImageUrl } from '../../../utils';
+import styles from "../Hero/Hero.module.css"
+
+const Hero = () => {
+    return (<section className={styles.container}>
+        <div className={styles.content}>
+            <h1 className={styles.title}>Hi ...text</h1>
+            <p className={styles.description}>Text........</p>
+            <a href="mailto:banchevpepo@gmail.com" className={styles.contanctBtn}>Contact Me</a>
+        </div>
+        <img src={getImageUrl("hero/cartoon-1.png")} alt='Hero image of me' className={styles.img} />
+        <div className={styles.topBlur} />
+        <div className={styles.bottomBlur} />
+    </section >
+    );
+}
+
+export default Hero
