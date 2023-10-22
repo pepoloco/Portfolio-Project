912f7005420869595d9f93a3147042f2759c1d1b
Pepo Banchev <banchevpepo@gmail.com>
2023-10-22 22:12:19 +0300

    update
    changed the navBar

--- Portfolio/src/components/Navbar/Navbar.jsx
+++ Portfolio/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react'
+<<<<<<< Updated upstream
 import styles from "../../components/Navbar/Navbar.module.css"
+=======
+import styles from './Navbar.module.css'
+>>>>>>> Stashed changes
 import { getImageUrl } from "../../utils"
-
 
 const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
+
     return (
+<<<<<<< Updated upstream
         <nav className={styles.navbar}>
             <a className={styles.title} href='/'>Portfolio</a>
             <div className={styles.menu}>
@@ -19,7 +24,34 @@ const Navbar = () => {
                 </ul>
             </div>
         </nav>
+=======
+        <nav className={styles.navbar_navBar}>
+            <a className={styles.title_navBar} href='/'>Portfolio</a>
+            <ul className={`${styles.menuItems} ${menuOpen ? styles.active : ''}`}>
+                <li>
+                    <a href='#about'>About</a>
+                </li>
+                <li>
+                    <a href='#experience'>Experience</a>
+                </li>
+                <li>
+                    <a href='#contact'>Contact</a>
+                </li>
+            </ul>
+            <div
+                className={styles.menu_navBar}
+                onClick={() => setMenuOpen(prev => !prev)}
+                aria-label="Menu"
+            >
+                <span className={styles.bar}></span>
+                <span className={styles.bar}></span>
+                <span className={styles.bar}></span>
+            </div>
+        </nav >
+>>>>>>> Stashed changes
     )
 }
+
+
 
 export default Navbar
