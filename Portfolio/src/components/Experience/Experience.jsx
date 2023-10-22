06e4268b9b6ff29d5fdaf5cbd1d306a16b060b30
Pepo Banchev <banchevpepo@gmail.com>
2023-10-22 18:35:06 +0300

    UPDATE
    Added Experience , will update later the json files with dates etc.

--- Portfolio/src/components/Experience/Experience.jsx
+++ Portfolio/src/components/Experience/Experience.jsx
@@ -6,10 +6,10 @@ import history from "../../data/history.json"
 
 const Experience = () => {
     return (
-        <section id='experience'>
-            <h2>Experience</h2>
-            <div>
-                <div>
+        <section className={styles.container_exp} id='experience'>
+            <h2 className={styles.title_exp}>Experience</h2>
+            <div className={styles.content_exp}>
+                <div className={styles.skills}>
                     {skills.map((skill, id) => {
                         return (
                             <div key={id} className={styles.skill}>
@@ -24,16 +24,16 @@ const Experience = () => {
                         );
                     })}
                 </div>
-                <ul>
+                <ul className={styles.history}>
                     {
                         history.map((historyItem, id) => {
                             return (
-                                <li key={id}>
+                                <li key={id} className={styles.historyItem}>
                                     <img
                                         src={getImageUrl(historyItem.imageSrc)}
                                         alt={`${historyItem.organisation} Logo`}
                                     />
-                                    <div>
+                                    <div className={styles.historyItemDetails}>
                                         <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
                                         <p>{`${historyItem.role}, ${historyItem.organisation}`}</p>
                                         <ul>{historyItem.experiences.map((experience, subId) => {
